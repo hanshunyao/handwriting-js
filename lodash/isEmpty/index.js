65f30b508467c6ecb9ccf3e8b04a9f68f446a67f
Hansy <hanshunyao_hansy@163.com>
2024-12-15 13:46:16 +0800

    修改 统一命名

--- lodash/isEmpty/index.js
+++ lodash/isEmpty/index.js
@@ -1,9 +1,9 @@
 // 实现 Lodash.isEmpty 功能
-function isEmpty (obj) {
+function myIsEmpty (obj) {
   return Object.keys(obj).length === 0;
 };
 
-console.log(isEmpty([1,2,3]));
-console.log(isEmpty([]));
-console.log(isEmpty({age:1}));
-console.log(isEmpty({}));
+console.log(myIsEmpty([1,2,3]));
+console.log(myIsEmpty([]));
+console.log(myIsEmpty({age:1}));
+console.log(myIsEmpty({}));
