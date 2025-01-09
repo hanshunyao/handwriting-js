be523d35a4f3d37790fc7a63ea396d386d88eeb5
Hansy <hanshunyao_hansy@163.com>
2025-01-09 23:00:15 +0800

    修改 补充利用 IntersectionObserver 实现图片懒加载

--- lazy-load-img/index.js
+++ lazy-load-img/index.js
@@ -1,6 +1,7 @@
 let imgList = [...document.querySelectorAll('img')];
 let length = imgList.length;
 
+// 1 监控滚动条实现懒加载
 const imgLazyLoad = (function () {
   let count = 0;
 
@@ -20,3 +21,29 @@ const imgLazyLoad = (function () {
     imgList = imgList.filter((img, index) => !deleteIndexList.includes(index));
   };
 })();
+
+
+// 2 利用 IntersectionObserver 实现图片懒加载
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    for(const entrie of entries){
+      if(entrie.isIntersection){
+        // 进入此分支，说明当前的图片和根元素产生了交叉
+        const img = entrie.target;
+        img.src = img.dataset.src;
+        observer.unobserve(img);
+      }
+    }
+  },
+  {
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 0.5
+  }
+);
+// 先拿到所有的图片元素
+const imgs = document.querySelectorAll("img");
+imgs.forEach((img) => {
+  //观察所有的图片元素
+  observer.observe(img);
+});
