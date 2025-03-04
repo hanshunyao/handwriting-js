ddc1e91201f8cd222a8be607fe8b9cc1a61b086d
hansy <hanshunyao_hansy@163.com>
2025-03-04 11:13:44 +0800

    优化 new 写法补全 增加判断

--- new/index.js
+++ new/index.js
@@ -1,18 +1,22 @@
-function myNew(func, ...args) {
-  // 步骤 1：创建一个空的简单 JavaScript 对象，即 { } ;
-  // 步骤 2：链接该对象到另一个对象（即设置该对象的原型对象）；
-  let obj = Object.create(func.prototype);
-  // 步骤 3：将步骤 1 新创建的对象作为 this 的上下文；
-  let result = func.apply(obj, args);
-  // 步骤 4：如果该函数没有返回对象，则返回 this。
-  if (
-    (typeof result === 'object' && result !== null) ||
-    typeof result === 'function'
-  ) {
-    return result;
-  } else {
-    return obj;
-  }
+// 使用 es5 语法版
+function objectFactory() {
+  var obj = new Object(),
+    Constructor = [].shift.call(arguments);
+  obj.__proto__ = Constructor.prototype;
+  var ret = Constructor.apply(obj, arguments);
+  return typeof ret === 'object' ? ret : obj;
+}
+
+// 使用 es6 语法版
+function myNew(constructor, ...args) {
+  // 1. 创建一个对象 let obj = new Object()
+  // 2. 对象的原型指向构造器的原型 obj.__proto__ = constructor.prototype
+  let obj = Object.create(constructor.prototype); // 两个步骤合并了
+  // 3. 取到构造器外剩下参数 es6 写法 直接收集了 args
+  // 4. 执行构造函数 收集返回值
+  let res = constructor.apply(obj, args);
+  // 5. 根据返回值 判断 返回值
+  return typeof res === 'object' ? res : obj;
 }
 
 // 调用
